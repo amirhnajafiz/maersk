3feaef38fa28d9e8e85fa2e183a88f4e0a7c1930
amirhnajafiz <najafizadeh21@gmail.com>
2022-11-25 20:29:50 +0330

    init: cargo

--- cargo.go
+++ cargo.go
@@ -1,1 +1,16 @@
 package maersk
+
+import "time"
+
+// Cargo
+// is the main module of maersk. In cargo the operation
+// of downloading chunks with workers is handled.
+type Cargo struct {
+	Out     string
+	URL     string
+	Mode    string
+	Workers int
+	Chunks  int
+	Timeout time.Duration
+	chunks  [][]byte
+}
