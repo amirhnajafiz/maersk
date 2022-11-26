b683ba9444c7a211098700ff96d3400658b55552
amirhnajafiz <najafizadeh21@gmail.com>
2022-11-26 15:13:38 +0330

    add: orders struct

--- order.go
+++ order.go
@@ -1,1 +1,27 @@
 package maersk
+
+import "time"
+
+// ShippingOrder
+// stores the information needed for creating cargo.
+type ShippingOrder struct {
+	Out     string
+	URL     string
+	Mode    string
+	Workers int
+	Chunks  int
+	Timeout time.Duration
+}
+
+// Build
+// generates a new cargo center with given orders.
+func Build(order *ShippingOrder) *Cargo {
+	return &Cargo{
+		Out:     order.Out,
+		URL:     order.URL,
+		Mode:    order.Mode,
+		Workers: order.Workers,
+		Chunks:  order.Chunks,
+		Timeout: order.Timeout,
+	}
+}
