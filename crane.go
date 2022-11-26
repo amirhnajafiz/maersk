fb9bd6f74215898da1adb2dfd9f4245d6b790205
amirhnajafiz <najafizadeh21@gmail.com>
2022-11-26 14:59:34 +0330

    add: crane module

---
+++ crane.go
@@ -0,0 +1,29 @@
+package maersk
+
+import "fmt"
+
+// crane
+// manages to keep track of jobs and chunks.
+// it resends the failed jobs into jobs channel again.
+type crane struct {
+	// jobs channel.
+	jobs chan job
+	// failed jobs channel.
+	failed chan job
+	// crane program for managing the failed jobs.
+	program func() error
+}
+
+// start
+// crane to work.
+func (c *crane) start() error {
+	for j := range c.failed {
+		c.jobs <- j
+
+		if err := c.program(); err != nil {
+			return fmt.Errorf("crane stopped: %v", err)
+		}
+	}
+
+	return nil
+}
