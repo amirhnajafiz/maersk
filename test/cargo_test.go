a9f531555fa460a4bf1afc92cd60d9be95b8e417
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-10 11:08:12 +0330

    add: tests

--- test/cargo_test.go
+++ test/cargo_test.go
@@ -1,9 +1,39 @@
 package test
 
-import "testing"
+import (
+	"os"
+	"testing"
+	"time"
+
+	"github.com/amirhnajafiz/maersk"
+)
 
 // TestCargo
 // testing cargo initialization and file downloading feature.
 func TestCargo(t *testing.T) {
-	t.Log("success")
+	url := "http://212.183.159.230/5MB.zip"
+	out := "5MB.zip"
+
+	defer func() {
+		if err := os.RemoveAll(out); err != nil {
+			t.Error(err)
+		}
+	}()
+
+	order := maersk.ShippingOrder{
+		Out:     out,
+		URL:     url,
+		Mode:    maersk.DEBUG,
+		Workers: 5,
+		Chunks:  5,
+		Timeout: 5 * time.Second,
+	}
+
+	center := maersk.Build(&order)
+
+	if err := center.Ship(); err != nil {
+		t.Error(err)
+	}
+
+	t.Log("succeed")
 }
